77ef395391e2c569007e6f9003afb3ea8fdc866c
Hakan Bilgin <hbi99@hotmail.com>
2020-11-26 02:10:19 +0100

    bugfixes

--- src/js/main.js
+++ src/js/main.js
@@ -2,123 +2,123 @@
 const tunes = {
 	async init() {
 		// fast references
-		this.content = window.find("content");
-		this.btnPlay = this.content.find(".btn-play");
-		this.btnPrev = this.content.find(".btn-prev");
-		this.btnNext = this.content.find(".btn-next");
-		this.btnRandom = this.content.find(".btn-random");
-		this.btnRepeat = this.content.find(".btn-repeat");
+		this.els = {
+			content: window.find("content"),
+			btnPlay: window.find(".btn-play"),
+			btnPrev: window.find(".btn-prev"),
+			btnNext: window.find(".btn-next"),
+			btnRandom: window.find(".btn-random"),
+			btnRepeat: window.find(".btn-repeat"),
+			songTitle: window.find(".title-bar"),
+			timePlayed: window.find(".time-played"),
+			timeTotal: window.find(".time-total"),
+			progressLoaded: window.find(".progress-loaded"),
+			progressPlayed: window.find(".progress-played"),
+			audio: window.find("audio"),
+		};
 
-		this.songTitle = this.content.find(".title-bar");
-		this.timePlayed = this.content.find(".time-played");
-		this.timeTotal = this.content.find(".time-total");
-
-		this.progressLoaded = this.content.find(".progress-loaded");
-		this.progressPlayed = this.content.find(".progress-played");
-
-		this.audio = this.content.find("audio");
-		this.music = this.audio[0];
+		this.music = this.els.audio[0];
 
 		// bind event handlers
-		this.audio
+		this.els.audio
 			.on("timeupdate", this.dispatch.bind(this))
 			.on("canplaythrough", this.dispatch.bind(this))
 			.on("progress", this.dispatch.bind(this));
 	},
 	async dispatch(event) {
-		let isOn,
+		let Self = tunes,
+			isOn,
 			time,
 			seconds,
 			minutes,
 			percentage,
 			value;
-		//console.log(event);
+		// console.log(event);
 		switch (event.type) {
-			// native events
-			case "progress":
-				for (var i = 0; i < this.music.buffered.length; i++) {
-					if (this.music.buffered.start(this.music.buffered.length - 1 - i) < this.music.currentTime) {
-						percentage = parseInt((this.music.buffered.end(this.music.buffered.length - 1 - i) / this.music.duration) * 100, 10);
-						break;
-					}
-				}
-				this.progressLoaded.css({width: percentage + "%"});
-				break;
-			case "timeupdate":
-				time = Math.round(this.music.currentTime);
-				seconds = (time % 60).toString().padStart(2, "0");
-				minutes = parseInt(time / 60, 10);
-				this.timePlayed.html(`${minutes}:${seconds}`);
-				// progress bar update
-				percentage = parseInt((this.music.currentTime / this.music.duration) * 100, 10);
-				if (percentage === 100) {
-					this.dispatch({type: "toggle-play"});
-					percentage = 0;
-				}
-				this.progressPlayed.css({width: percentage + "%"});
-				break;
-			case "canplaythrough":
-				time = Math.round(this.music.duration);
-				seconds = (time % 60).toString().padStart(2, "0");
-				minutes = parseInt(time / 60, 10);
-				this.timeTotal.html(`${minutes}:${seconds}`);
-
-				if (this.skipPlayThrough) return;
-				// reset time played
-				this.timePlayed.html(`0:00`);
-				break;
 			// custom events
 			case "window.open":
-				//this.audio.attr({src: "/fs/usr/mp3/Eclectek_-_02_-_We_Are_Going_To_Eclecfunk_Your_Ass.mp3"});
-				//this.dispatch({type: "window.maximize"});
+				//Self.audio.attr({src: "/fs/usr/mp3/Eclectek_-_02_-_We_Are_Going_To_Eclecfunk_Your_Ass.mp3"});
+				//Self.dispatch({type: "window.maximize"});
 				break;
 			case "window.close":
-				if (this.btnPlay.hasClass("playing")) this.dispatch({type: "toggle-play"});
+				if (Self.els.btnPlay.hasClass("playing")) Self.dispatch({type: "toggle-play"});
 				break;
 			case "open.file":
-				if (this.btnPlay.hasClass("playing")) this.dispatch({type: "toggle-play"});
-				this.songTitle.html(event.name.slice(0, event.name.lastIndexOf(".")));
-				this.audio.attr({src: event.path});
-				this.dispatch({type: "toggle-play"});
+				if (Self.els.btnPlay.hasClass("playing")) Self.dispatch({type: "toggle-play"});
+				Self.els.songTitle.html(event.base);
+				Self.els.audio.attr({src: event.path});
+				Self.dispatch({type: "toggle-play"});
 				break;
 			case "window.restore":
-				this.content.removeClass("extended");
+				Self.els.content.removeClass("extended");
 				return false;
 			case "window.maximize":
 				window.height = "auto";
-				this.content.addClass("extended");
+				Self.els.content.addClass("extended");
 				return false;
-			// custom click events
+			// custom events
+			case "progress":
+				for (var i = 0; i < Self.music.buffered.length; i++) {
+					if (Self.music.buffered.start(Self.music.buffered.length - 1 - i) < Self.music.currentTime) {
+						percentage = parseInt((Self.music.buffered.end(Self.music.buffered.length - 1 - i) / Self.music.duration) * 100, 10);
+						break;
+					}
+				}
+				Self.els.progressLoaded.css({width: percentage + "%"});
+				break;
+			case "timeupdate":
+				time = Math.round(Self.music.currentTime);
+				seconds = (time % 60).toString().padStart(2, "0");
+				minutes = parseInt(time / 60, 10);
+				Self.els.timePlayed.html(`${minutes}:${seconds}`);
+				// progress bar update
+				percentage = parseInt((Self.music.currentTime / Self.music.duration) * 100, 10);
+				if (percentage === 100) {
+					Self.dispatch({type: "toggle-play"});
+					percentage = 0;
+				}
+				Self.els.progressPlayed.css({width: percentage + "%"});
+				break;
+			case "canplaythrough":
+				time = Math.round(Self.music.duration);
+				seconds = (time % 60).toString().padStart(2, "0");
+				minutes = parseInt(time / 60, 10);
+				Self.els.timeTotal.html(`${minutes}:${seconds}`);
+
+				if (Self.skipPlayThrough) return;
+				// reset time played
+				Self.els.timePlayed.html(`0:00`);
+				break;
 			case "progress-seek":
 				let rect = event.el[0].getBoundingClientRect(),
 					left = event.clientX - rect.left;
 
-				this.skipPlayThrough = true;
-				time = this.music.duration * (left / event.el.width());
-				this.music.currentTime = time;
+				Self.skipPlayThrough = true;
+				time = Self.music.duration * (left / event.el.width());
+				Self.music.currentTime = time;
 
 				setTimeout(() => tunes.skipPlayThrough = false, 10);
 				break;
 			case "play-prev":
 				break;
 			case "toggle-play":
-				if (this.btnPlay.hasClass("playing")) {
-					this.music.pause();
-					this.btnPlay.removeClass("playing");
+				if (Self.els.btnPlay.hasClass("playing")) {
+					Self.music.pause();
+					Self.els.btnPlay.removeClass("playing");
 				} else {
-					this.music.play();
-					this.btnPlay.addClass("playing");
+					Self.music.play();
+					Self.els.btnPlay.addClass("playing");
 				}
 				break;
 			case "play-next":
 				break;
 			case "play-random":
-				isOn = this.btnRandom.hasClass("active");
-				this.btnRandom.toggleClass("active", isOn);
+				isOn = Self.els.btnRandom.hasClass("active");
+				Self.els.btnRandom.toggleClass("active", isOn);
 				return !isOn;
 			case "play-repeat":
-				isOn = this.btnRepeat.hasClass("active");
-				this.btnRepeat.toggleClass("active", isOn);
+				isOn = Self.els.btnRepeat.hasClass("active");
+				Self.els.btnRepeat.toggleClass("active", isOn);
 				return !isOn;
 		}
 	}
