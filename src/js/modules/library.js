e9828163a4c28246937e533ff687645ddd3be957
Hakan Bilgin <hbi99@hotmail.com>
2023-09-30 18:54:18 +0200

    processing data with library module

--- src/js/modules/library.js
+++ src/js/modules/library.js
@@ -4,9 +4,7 @@
 {
 	init() {
 		karaqu.shell(`fs -k mp3`)
-			.then(res => {
-				console.log(res);
-			});
+			.then(res => this.dispatch({ type: "parse-music-files", list: res.result }));
 	},
 	dispatch(event) {
 		let APP = tunes,
@@ -14,8 +12,21 @@
 			el;
 		// console.log(event);
 		switch (event.type) {
-			case "init":
-				
+			case "parse-music-files":
+				event.list.map(file => {
+					let id = file.path.sha1(),
+						xNode = window.bluePrint.selectSingleNode(`//Data//*[@id="${id}"]`);
+					if (xNode) {
+						let name = file.name.endsWith(".mp3") ? file.name.slice(0,-4) : file.name;
+						if (name.includes(" - ")) {
+							let [artist, title] = name.split(" - ");
+							xNode.setAttribute("artist", artist);
+							xNode.setAttribute("title", title);
+						}
+						xNode.setAttribute("name", name);
+					}
+					// console.log(file);
+				});
 				break;
 		}
 	}
