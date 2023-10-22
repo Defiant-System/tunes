383405c2cb25cf19229bef126366f34e4d313fea
Hakan Bilgin <hbi99@hotmail.com>
2023-10-22 19:05:06 +0200

    dark theme

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -2,10 +2,18 @@
 let Test = {
 	init(APP) {
 		
-		let file = new karaqu.File({ kind: "txt" });
-		window.dialog.saveAs(file, { txt: r => console.log(r) });
+		// setTimeout(() => 
+		// 	window.find(".sample-view").css({ background: `url(/cdn/img/bg/abstract/galaxy-s7.jpg) 50% 50%` })
+		// 	, 300);
 
-		// setTimeout(() => window.find(`.sample-view .btn[data-click="open-filesystem"]`).trigger("click"), 200);
+		// let file = new karaqu.File({ kind: "txt" });
+		// window.dialog.saveAs(file, { txt: r => console.log(r) });
+
+		setTimeout(() => window.find(`.sample-view .btn[data-click="open-filesystem"]`).trigger("click"), 200);
+		
+		// setTimeout(() => window.find(`.ant-file_:nth(3)`).trigger("mousedown").trigger("mouseup"), 900);
+		// setTimeout(() => window.find(`.fs-folder-select > div[selected]`).trigger("click"), 900);
+
 		// setTimeout(() => window.find(`.new-playlist`).trigger("click"), 200);
 		// setTimeout(() => window.find(`sidebar li:nth(7)`).trigger("click"), 200);
 		// setTimeout(() => window.find(`.toolbar-tool_[data-click="play-toggle"]`).trigger("click"), 300);
